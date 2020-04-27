1dd4dc37627dbbcbc6c81bcea74813ad120d6430
teekaytech <taofeek.olalere@feghas.com>
2020-04-27 23:38:42 +0100

    Fix linter issues in my_each and my_each_with_index methods

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ module Enumerable
     return 'No block given' unless block_given?
 
     index = 0
-    while index < self.size
+    while index < size
       if is_a?(Array)
         yield(self[index])
       elsif is_a?(Hash)
@@ -21,7 +21,7 @@ module Enumerable
     return 'No block given' unless block_given?
 
     index = 0
-    while index < self.size
+    while index < size
       if is_a?(Array)
         yield(self[index], index)
       elsif is_a?(Hash)
@@ -39,14 +39,13 @@ module Enumerable
   my_ha = { mine: 1, yours: 2 }
   my_ra = 1..5
 
-
-  puts "Testing my_each"
+  puts 'Testing my_each'
   my_ra.my_each { |item| puts "#{item} is here" }
   my_arr.my_each { |item| puts item }
   my_ha.my_each { |key, item| puts "#{key} => #{item}" }
 
-  puts "Testing my_each_with_index"
-  my_ra.my_each_with_index { |item, i| puts "#{item} is here with #{i+1}" }
+  puts 'Testing my_each_with_index'
+  my_ra.my_each_with_index { |item, i| puts "#{item} is here with #{i + 1}" }
   my_arr.my_each_with_index { |item, i| puts "#{item} with #{i}" }
   my_ha.my_each_with_index { |key, item| puts "#{key} => #{item}" }
 end
