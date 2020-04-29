54b0ee314dfaf493b3258a0597ca279fa5df2725
teekaytech <taofeek.olalere@feghas.com>
2020-04-29 12:52:31 +0100

    Fix linter (rubocop errors) and test after fixing the main.rb file.

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
-module Enumerable
+# rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+module Enumerable # rubocop:disable Metrics/ModuleLength
   def my_each
     return to_enum(:my_each) unless block_given?
 
@@ -40,109 +41,79 @@ module Enumerable
 
     if is_a?(Array) || is_a?(Range)
       selected_array = []
-      self.my_each { |item| selected_array.push(item) if yield(item) }
-      return selected_array
+      my_each { |item| selected_array.push(item) if yield(item) }
+      selected_array
     else
       selected_hash = {}
-      self.my_each { |key, value| selected_hash[key] = value if yield(key, value) }
-      return selected_hash
+      my_each { |key, value| selected_hash[key] = value if yield(key, value) }
+      selected_hash
     end
   end
 
-
   def my_all(my_arg)
-    if my_arg.class == Regexp 
-      self.my_all? { |item| item =~ my_arg }
+    if my_arg.class == Regexp
+      my_all? { |item| item =~ my_arg }
     elsif my_arg.class == Class
-      self.my_all? { |item| item.is_a? my_arg }
-    else 
-      self.my_all? { |item| item == my_arg }
+      my_all? { |item| item.is_a? my_arg }
+    else
+      my_all? { |item| item == my_arg }
     end
   end
 
   def my_all?(my_arg = nil)
-    return my_all(my_arg) unless my_arg == nil
+    return my_all(my_arg) unless my_arg.nil?
 
     if block_given?
-      if is_a?(Array) || is_a?(Range)
-        self.my_each { |item| return false if yield(item) == false || yield(item) == nil }
-      else
-        self.my_each { |key, item| return false if (yield(key, item) == true || yield(item) == nil) }
-      end
+      my_each { |item| return false if yield(item) == false || yield(item).nil? }
     else
-      self.my_each { |item| return false if item == false || item == nil }
+      my_each { |item| return false if item == false || item.nil? }
     end
     true
   end
 
   def my_any?(my_arg = nil)
-    if block_given? && my_arg == nil
+    if block_given? && my_arg.nil?
       if is_a?(Array) || is_a?(Range)
-        self.my_each { |item| return true if yield(item) == true }
+        my_each { |item| return true if yield(item) == true }
       else
-        self.my_each { |key, item| return true if yield(key, item) == true }
+        my_each { |key, item| return true if yield(key, item) == true }
       end
       false
     end
     if my_arg == Regexp
-      self.my_each { |item| return true if item.to_s.match(my_arg) }
+      my_each { |item| return true if item.to_s.match(my_arg) }
     end
-    if my_arg.is_a?Class
-      self.my_each { |item| return true if item.is_a?my_arg }
+    if my_arg.is_a? Class
+      my_each { |item| return true if item.is_a? my_arg }
     end
-    if my_arg == nil
-      self.my_each { |item| return true if item == true }
+    if my_arg.nil?
+      my_each { |item| return true if item == true }
     end
     false
   end
 
   def my_none?(my_arg = nil)
-    if block_given? && my_arg == nil
-      if is_a?(Array) || is_a?(Range)
-        self.my_each { |item| return false if yield(item) == true }
-      else
-        self.my_each { |key, item| return false if yield(key, item) == true }
-      end
-      true
+    is_none = true
+    if block_given?
+      my_each { |item| is_none = false if yield(item) == true }
+    elsif my_arg == Regexp
+      my_each { |item| is_none = false if item.to_s.match(my_arg) }
+    elsif my_arg.is_a? Class
+      my_each { |item| is_none = false if item.is_a? my_arg }
+    else
+      my_each { |item| is_none = false if item == true }
     end
-    if my_arg == Regexp
-      self.my_each { |item| return false if item.to_s.match(my_arg) }
-    end
-    if my_arg.is_a?Class
-      self.my_each { |item| return false if item.is_a?my_arg }
-    end
-    if my_arg == nil
-      self.my_each { |item| return false if item == true }
-    end
-    true
+    is_none
   end
 
   def my_count(params = nil)
     counter = 0
-    if block_given? 
-      if self.is_a?(Array) || self.is_a?(Range)
-        self.my_each do |item|
-          counter += 1 if yield(item) == true
-        end
-      else #for Hashes
-        self.my_each do |key, item| 
-          counter += 1 if yield(key, item) == true
-        end
-      end
+    if block_given?
+      my_each { |item| counter += 1 if yield(item) }
+    elsif params.nil?
+      counter = to_a.length
     else
-      if self.is_a?(Array) || self.is_a?(Range)
-        self.my_each do |item|
-          counter += 1
-        end
-      else #for Hashes
-        self.my_each do |key, item| 
-          counter += 1
-        end
-      end
-    end
-    if params != nil
-      counter = 0
-      self.my_each { |item| counter += 1 if params == item }
+      my_each { |item| counter += 1 if params == item }
     end
     counter
   end
@@ -152,35 +123,26 @@ module Enumerable
 
     if proc.class == Proc
       mapped_array = []
-      self.my_each { |item| mapped_array.push(proc.call(item)) }
+      my_each { |item| mapped_array.push(proc.call(item)) }
       mapped_array
-    else 
-      if is_a?(Array) || is_a?(Range)
-        selected_array = []
-        self.my_each { |item| selected_array.push(yield(item)) }
-        return selected_array
-      else
-        selected_hash = {}
-        self.my_each { |key, value| selected_hash[key] = yield(key, value) }
-        return selected_hash
-      end
+    elsif is_a?(Array) || is_a?(Range)
+      selected_array = []
+      my_each { |item| selected_array.push(yield(item)) }
+      selected_array
+    else
+      selected_hash = {}
+      my_each { |key, value| selected_hash[key] = yield(key, value) }
+      selected_hash
     end
   end
 
   def my_inject(initial = 0)
     return to_enum(:my_each) unless block_given?
-    acc = initial #captures initialization of the accumulator
-      if self.is_a?(Array) || self.is_a?(Range)
-        self.my_each { |item| acc = yield(acc, item) }
-      end
-      acc
-  end
 
+    acc = initial # aptures initialization of the accumulator
+    my_each { |item| acc = yield(acc, item) } if is_a?(Array) || is_a?(Range)
+    acc
+  end
 end
 
-p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-p %w[ant bear cat].my_all?(/t/)                        #=> false
-p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
-p [nil, true, 99].my_all?                              #=> false
-p [].my_all?                                           #=> true
+# rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
