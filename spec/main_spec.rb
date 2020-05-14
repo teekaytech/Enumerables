a12ec5ce7a10408bd6518dd2447b74a7e09bca59
teekaytech <taofeek.olalere@feghas.com>
2020-05-14 12:56:11 +0100

    Add specs for my_each_with_index and my_select methods

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,7 +1,7 @@
 require './main'
 
 describe Enumerable do
-  let(:numeric_values) { [1, 2, 3, 4, 5] }
+  let(:array) { [1, 2, 3, 4, 5] }
   let(:mixed_values) { [1, 2, 3, 4, a] }
   let(:string_values) { %w[ant bear cat] }
   let(:float_values) { [1, 3.14, 42] }
@@ -9,12 +9,59 @@ describe Enumerable do
   let(:nil_array) { [nil] }
   let(:nil_false_array) { [nil, false] }
   let(:nil_false_true) { [nil, false, true] }
+  let(:hash) { { 'color' => '#fff', 'font_family' => 'Arial' } }
 
   describe '#my_each' do
-    it 'returns each array items if block is given' do
+    it 'returns each items of array/range if block is given' do
       result = []
-      numeric_values.my_each { |item| result << item + 1 }
+      array.my_each { |item| result << item + 1 }
       expect(result).to eq([2, 3, 4, 5, 6])
+    end
+
+    it 'returns keys and values of hash if block is given' do
+      result = []
+      hash.my_each { |key, value| result << key + ' : ' + value }
+      expect(result).to eq(["color : #fff", "font_family : Arial"])
+    end
+
+    it 'returns the enumerator data if block is not given' do
+      expect(array.my_each).to be_an(Enumerator)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'returns values and keys of array/range if block is given' do
+      result = []
+      array.my_each_with_index { |item, index| result << item + index }
+      expect(result).to eq([1, 3, 5, 7, 9])
+    end
+
+    it 'returns values and keys of hash if block is given' do
+      result = []
+      hash.my_each_with_index { |value, key| result << value + ' : ' + key }
+      expect(result).to eq(["color : #fff", "font_family : Arial"])
+    end
+
+    it 'returns the enumerator data if block is not given' do
+      expect(array.my_each_with_index).to be_an(Enumerator)
+    end
+  end
+
+  describe '#my_select' do
+    context 'if block is given returns true' do
+      it 'returns an array containing all elements of array' do
+        result = []
+        array.my_select { |item| result << item if item.even? }
+        expect(result).to eq([2, 4])
+      end
+
+      it 'returns a hash containing all elements/keys of hash' do
+        result = hash.my_select { |val| val > 'a' }
+        expect(result).to eq({ 'color' => '#fff', 'font_family' => 'Arial' })
+      end
+    end
+    it 'returns the enumerator data if block is not given' do
+      expect(array.my_select).to be_an(Enumerator)
     end
   end
 end
