754978c140fe397bb2b39834cd1109cf1dafcae5
teekaytech <taofeek.olalere@feghas.com>
2020-05-14 14:36:48 +0100

    Add test cases for #my_any? and my_none? methods

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,10 +2,9 @@ require './main'
 
 describe Enumerable do
   let(:array) { [1, 2, 3, 4, 5] }
-  let(:mixed_val) { [1, 2, 3, 4, a] }
+  let(:mixed_val) { [1, 2, 3, 4, 'a'] }
   let(:string_val) { %w[ant bear cat] }
   let(:float_val) { [1, 3.14, 42] }
-  let(:empty_array) { [] }
   let(:nil_array) { [nil] }
   let(:nil_false_array) { [nil, false] }
   let(:nil_false_true) { [nil, false, true] }
@@ -98,6 +97,76 @@ describe Enumerable do
 
       it 'return true if param == pattern and pattern === element for all elements' do
         expect(string_val.my_all?(/t/)).to eq(false)
+      end
+    end
+  end
+
+  describe '#my_any?' do
+    context 'if block is given' do
+      it 'returns true if block returns true for any of the elements of the array/range' do
+        result = string_val.my_any? { |item| item.length >= 3 }
+        expect(result).to eq(true)
+      end
+
+      it 'returns true if block returns true for any of the elements/keys of the hash' do
+        result = hash.my_any? { |item, key| item.length >= 3 && key.length >= 3 }
+        expect(result).to eq(true)
+      end
+
+      it 'returns false if block returns false for all elements' do
+        expect(array.my_any? { |i| i == 6 }).to eq(false)
+      end
+    end
+
+    context 'if block is not given and param is not given' do
+      it 'return true if at least one of the collection members is not false or nil' do
+        expect([nil, true, 99].my_any?).to eq(true)
+      end
+
+      it 'return false if collection has no member ' do
+        expect([].my_any?).to eq(false)
+      end
+    end
+
+    context 'if block is not given but param is given' do
+      it 'return false if param == class and all elements is not a member of param given' do
+        expect(string_val.my_all?(Integer)).to eq(false)
+      end
+
+      it 'return true if param == pattern and pattern === element for all elements' do
+        expect(string_val.my_any?(/d/)).to eq(false)
+      end
+    end
+  end
+
+  describe '#my_none?' do
+    context 'if block is given' do
+      it 'returns true if the block never returns true for all elements' do
+        expect(string_val.my_none? { |item| item.length >= 7 }).to eq(true)
+      end
+
+      it 'returns false if block returns true for any of the collection members' do
+        expect(array.my_none? { |i| i == 3 }).to eq(false)
+      end
+    end
+
+    context 'if block is not given and param is not given' do
+      it 'return true only if none of the collection members is true' do
+        expect(nil_false_array.my_none?).to eq(true)
+      end
+
+      it 'return true for empty collection' do
+        expect([].my_none?).to eq(true)
+      end
+    end
+
+    context 'if block is not given but param is given' do
+      it 'return false if param == class and at least one collection instance is a member of the param' do
+        expect(float_val.my_none?(Float)).to eq(false)
+      end
+
+      it 'return true if param == pattern and pattern === element for none of the collection members' do
+        expect(string_val.my_none?(/d/)).to eq(true)
       end
     end
   end
